8d5176d7f47dc6221c143c8802f7b7f960ffde14
elissonguerra <elisson.guerra@gmail.com>
2024-08-09 22:24:23 -0300

    Adicionado o comando para colocar imagens na lista

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import {
   StyleSheet,
   Text,
   StatusBar,
+  Image
 } from 'react-native';
 import {List} from 'react-native-paper'
 
@@ -27,13 +28,13 @@ const DATA = [
   },
 ];
 
-const Item = ({title}) => {
-  console.warn(title)
+const Item = ({elemento: item}) => {
   return(
   <List.Item
-    title={title.titulo}
-    description={title.descricao}
-    left={props => <List.Icon {...props} icon="folder" />}
+    title={item.titulo}
+    description={item.descricao}
+    // left={props => <List.Icon {...props} icon="folder" />}
+    left={props => <Image {...props} style={styles.tinyLogo}source={{uri: 'https://cdn.pixabay.com/photo/2016/11/19/13/57/drum-set-1839383_1280.jpg'}}/>}
   />
 )
 };
@@ -43,7 +44,7 @@ const App = () => {
     <SafeAreaView style={styles.container}>
       <FlatList
         data={DATA}
-        renderItem={({item}) => <Item title={item} />}
+        renderItem={({item}) => <Item elemento={item} />}
         keyExtractor={item => item.id}
       />
     </SafeAreaView>
@@ -64,6 +65,10 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 32,
   },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  }
 });
 
 export default App;

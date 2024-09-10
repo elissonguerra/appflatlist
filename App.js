66b7bba25ab8ed37d7923df362c39b7a6838a473
elissonguerra <elisson.guerra@gmail.com>
2024-09-10 20:07:29 -0300

    Atualizado o projeto

--- App.js
+++ App.js
@@ -1,74 +1,21 @@
-import React from 'react';
-import {
-  SafeAreaView,
-  View,
-  FlatList,
-  StyleSheet,
-  Text,
-  StatusBar,
-  Image
-} from 'react-native';
-import {List} from 'react-native-paper'
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ListaProdutoScreen from './components/ListaProdutoScreen';
+import DetalheProdutoScreen from './components/DetalheProdutoScreen';
 
-const DATA = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    titulo: 'Primeiro Item',
-    descricao: 'Descrição do Primeiro Item'
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    titulo: 'Segundo Item',
-    descricao: 'Descrição do Segundo Item'
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    titulo: 'Terceiro Item',
-    descricao: 'Third Item Description'
-  },
-];
+const Stack = createNativeStackNavigator();
 
-const Item = ({elemento: item}) => {
-  return(
-  <List.Item
-    title={item.titulo}
-    description={item.descricao}
-    // left={props => <List.Icon {...props} icon="folder" />}
-    left={props => <Image {...props} style={styles.tinyLogo}source={{uri: 'https://cdn.pixabay.com/photo/2016/11/19/13/57/drum-set-1839383_1280.jpg'}}/>}
-  />
-)
-};
-
-const App = () => {
+function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={DATA}
-        renderItem={({item}) => <Item elemento={item} />}
-        keyExtractor={item => item.id}
-      />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='ListaProduto'>
+        <Stack.Screen name="ListaProduto" component={ListaProdutoScreen} />
+        <Stack.Screen name='DetalheProduto' component={DetalheProdutoScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-  },
-  item: {
-    backgroundColor: '#f9c2ff',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-  },
-  title: {
-    fontSize: 32,
-  },
-  tinyLogo: {
-    width: 50,
-    height: 50,
-  }
-});
+}
 
 export default App;
